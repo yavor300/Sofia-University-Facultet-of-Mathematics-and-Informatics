47e20b828f134eeb16c18349be5c39798465cb56
Yavor Chamov <yavor300@gmail.com>
2026-05-26 08:47:04 +0000

    [Knowledge-Discovery-in-Text] (Week-14) Add second homework.

---
+++ Year-1/Semester-2/Knowledge-Discovery-in-Text/Homework-02/openqa-imageclef-2026-textual/cache_setup.sh
@@ -0,0 +1,4 @@
+export HF_HOME=/workspace/.cache/huggingface
+export HF_DATASETS_CACHE=/workspace/.cache/huggingface/datasets
+export TRANSFORMERS_CACHE=/workspace/.cache/huggingface
+export PIP_CACHE_DIR=/tmp/pip-cache
